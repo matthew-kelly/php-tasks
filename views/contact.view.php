33f05a1b12efd23b866e7639cca3d5bab484dc20
Matt Kelly <bmatthewkelly@gmail.com>
2018-10-27 13:37:32 -0700

    create DRY templates

--- views/contact.view.php
+++ views/contact.view.php
@@ -1,12 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Contact</title>
-</head>
-<body>
+<?php require('partials/head.php'); ?>
+<header>
   <h1>Contact Us</h1>
-</body>
-</html>
+</header>
+<?php require('partials/footer.php'); ?>
--- views/index.view.php
+++ views/index.view.php
@@ -1,36 +1,14 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Home</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <style>
-      header {
-        background: #e3e3e3;
-        padding: 2em;
-        text-align: center;
-      }
-    </style>
-  </head>
-  <body>
-  <header>
-    <h1>Home</h1>
-  </header>
-  <nav>
-    <ul>
-      <li><a href="/about">About Us</a></li>
-      <li><a href="/contact">Contact Us</a></li>
-    </ul>
-  </nav>
-    <?php foreach ($tasks as $task) : ?>
-      <li>
-        <?php if ($task->completed) : ?>
-          <strike><?= $task->description; ?></strike>
-        <?php else : ?>
-          <?= $task->description; ?>
-        <?php endif; ?>
-      </li>
-    <?php endforeach; ?>
-  </body>
-</html>
+<?php require('partials/head.php'); ?>
+<header>
+  <h1>Home</h1>
+</header>
+<?php foreach ($tasks as $task) : ?>
+  <li>
+    <?php if ($task->completed) : ?>
+      <strike><?= $task->description; ?></strike>
+    <?php else : ?>
+      <?= $task->description; ?>
+    <?php endif; ?>
+  </li>
+<?php endforeach; ?>
+<?php require('partials/footer.php'); ?>
